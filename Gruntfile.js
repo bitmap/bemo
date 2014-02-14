84bf9f80ae4e725bc56b0beda875572e69dc8806
Cabe <caleb.branson@gmail.com>
2014-02-14 14:12:10 -0800

    fixing package.json error, single-quote gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,13 +67,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('default', ['sass', 'cssmin']);
-  grunt.registerTask("dev", [
-    "connect", 
-    "watch"
-  ]);
+  grunt.registerTask('dev', ['connect', 'watch']);
 
 };
