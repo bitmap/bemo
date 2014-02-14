61385c5989e2cb40c156b6582155968d2c9c1219
Cabe <caleb.branson@gmail.com>
2014-02-14 13:42:47 -0800

    adding grunt tasks "watch" and "connect"

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,13 +30,50 @@ module.exports = function(grunt) {
       strict: {
         src: ['bemo.css']
       }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      html: {
+        files: ['index.html']
+      },
+      js: {
+        files: ['js/*.js']
+        //tasks: ['jshint'] //todo
+      },
+      sass: {
+        options: {
+          livereload: false,
+        },
+        files: ['css/bemo.scss'],
+        tasks: ['sass']
+      },
+      css: {
+        files: ['bemo.css'],
+        tasks: ['cssmin', 'csslint']
+      },
+    },
+    connect: {
+      port: {
+        options: {
+          port: 35729,
+          base: ''
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-connect");
 
   grunt.registerTask('default', ['sass', 'cssmin']);
+  grunt.registerTask("dev", [
+    "connect", 
+    "watch"
+  ]);
 
 };
